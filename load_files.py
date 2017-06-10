48c7acadf23c159ba773708de5fcb0ff1cf95e11
sachablade <juanma.egm31@gmail.com>
2017-06-10 20:04:48 +0200

    1

---
+++ load_files.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+import time
+from properties import read_properties
+
+properties=read_properties()
+print properties
+time.sleep(60)
+
---
+++ properties/__init__.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+import os
+from dateutil.parser import parse
+
+
+def read_properties():
+    try:
+        azkaban_job_prop_file = os.environ["JOB_PROP_FILE"]
+    except KeyError:
+        return None
+
+    with open(azkaban_job_prop_file) as f:
+        content = f.readlines()
+    d = [x.strip().split("=") for x in content]
+    dict = {}
+    for item in d:
+        if len(item) == 2:
+            dict[item[0]] = item[1]
+
+    dict['azkaban.flow.start.timestamp'] = parse(
+        dict['azkaban.flow.start.timestamp'].replace("\\", "")[:25])
+
+    print ('JOB_NAME:%s '%dict['azkaban.job.id'])
+
+    return dict
+
