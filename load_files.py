4bc573377a02e133d1faff17a242c3a878b240b2
Juan Manuel Cristóbal <juanma.egm31@gmail.com>
2017-06-10 21:19:21 +0200

    1

--- load_files.py
+++ load_files.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python
 import time
+import json
 from properties import read_properties
 
 properties,out_properties_file=read_properties()
 print properties
 time.sleep(60)
 
-with open(out_properties_file, "a") as myfile:
-    myfile.write("mijob=valorrrrrrr")
+
+with open(out_properties_file) as json_file:
+    json_decoded = json.load(json_file)
+
+json_decoded['ADDED_KEY'] = 'ADDED_VALUE'
+
+with open(json_file, 'w') as json_file:
+    json.dump(json_decoded, json_file)
 
 print 'SE HAN CARGADO TODOS LOS FICHEROS'
--- process_files.py
+++ process_files.py
@@ -6,7 +6,4 @@ properties,out_properties_file=read_properties()
 print properties
 time.sleep(60)
 
-with open(out_properties_file, "a") as myfile:
-    myfile.write("mijob=valorrrrrrr")
-
 print 'SE HAN CARGADO TODOS LOS FICHEROS'
