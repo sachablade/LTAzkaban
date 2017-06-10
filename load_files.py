d3c7cf16a2dc2c8b47c8bf0ac760395c1300f9ab
Juan Manuel Cristóbal <juanma.egm31@gmail.com>
2017-06-10 21:25:53 +0200

    1

--- load_files.py
+++ load_files.py
@@ -5,20 +5,14 @@ from properties import read_properties
 
 properties,out_properties_file=read_properties()
 print properties
-time.sleep(60)
+#time.sleep(60)
 
 print out_properties_file
-with open(out_properties_file) as f:
-    content = f.readlines()
-# you may also want to remove whitespace characters like `\n` at the end of each line
-print [x.strip() for x in content]
 
-with open(out_properties_file) as json_file:
-    json_decoded = json.load(json_file)
-
+json_decoded={}
 json_decoded['ADDED_KEY'] = 'ADDED_VALUE'
 
-with open(json_file, 'w') as json_file:
+with open(out_properties_file, 'w') as json_file:
     json.dump(json_decoded, json_file)
 
 print 'SE HAN CARGADO TODOS LOS FICHEROS'
