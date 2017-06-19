950ee10d151d0a883502d3b4d1e80e5ff4bddd42
Juan Manuel Cristóbal <juanma.egm31@gmail.com>
2017-06-19 11:57:20 +0200

    1

--- load_files.py
+++ load_files.py
@@ -1,12 +1,28 @@
 #!/usr/bin/python
-import time
-import json
+
+import argparse
+
 from properties import read_properties
 from properties import write_properties
 
 properties=read_properties()
 print properties
-time.sleep(20)
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-f', '--from_date', help="fecha inicio de la carga "
+                                              "de datos (inclusive).  "
+                                              "formato yyyy-mm-dd",
+                    required=False)
+
+parser.add_argument('-t', '--to_date',
+                    help="fecha fin de la carga de datos (inclusive). "
+                         "formato yyyy-mm-dd or 'now' ",
+                    required=False)
+
+args = parser.parse_args()
+
+print
 
 json_decoded={}
 json_decoded['PARAMETROS'] = 'PARAMETRO PASADO ENTRE PROCESOS'
@@ -14,3 +30,4 @@ json_decoded['PARAMETROS'] = 'PARAMETRO PASADO ENTRE PROCESOS'
 write_properties(json_decoded)
 
 print 'SE HAN CARGADO TODOS LOS FICHEROS'
+
--- process_files.py
+++ process_files.py
@@ -4,9 +4,9 @@ from properties import read_properties
 
 properties=read_properties()
 print properties
-time.sleep(20)
+
 
 print properties['PARAMETROS']
 print 'SE HAN CARGADO TODOS LOS FICHEROS'
 
-raise Exception('a ver que pasa')
+#raise Exception('a ver que pasa')
